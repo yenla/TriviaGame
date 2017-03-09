4f0742f0e3fde450f2f67f4f913a76779d19d5b2
Skyler La <yenla@Skylers-MacBook-Pro.local>
2017-03-08 17:50:25 -0800

    Add more jquery for the game and a readme file

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # TriviaGame
+
+
+HW - Trivia Game
+
+
+Live Link 
+
+https://yenla.github.io/TriviaGame/
+
+
+Summary
+
+• In this homework assignment, I have to create a time trivia game. I choose a Disney theme because I love disney and its also a simple topic for the user to answer. The requirement for this homework is to have a timer, each question will have the same amount of time. User will need to choose an answer to move on to the next question. If the time run out and the user haven't pick an answer yet, the trivia will still automatically move on to the next question.
+
+
+Code Explaination
+
+I use an on click event to start of the game follow by the question and the answers to displaying on the screen. The choices will display in a form of button which the user can click to move on to the next question.
+
+For example:
+
+	function displayTrivia() {
+
+    $("#question_div").html(disneyQuestion[0].question);
+    question++;
+    var choicesArr = disneyQuestion[0].choices;
+
+    var buttonsArr = [];
+
+      for (let i = 0; i < choicesArr.length; i++) {
+        var button = $('<button>');
+        button.text(choicesArr[i]);
+        button.attr('data-id', i);
+        $('#choices_div').append(button);
+      }
+  }
+
+    $('#choices_div').on('click', 'button', function(e){
+    userPick = $(this).data("id");
+
+     if(userPick != disneyQuestion[0].validAnswer) {
+        $('#choices_div').text("Wrong Answer! The correct answer is Rajah.");
+        incorrectAnswer++;
+      } else if (userPick === disneyQuestion[0].validAnswer) {
+        $('#choices_div').text("Correct!!! The pet tiger name is Rajah.");
+        correctAnswer++;
+      }
+  });
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,44 +8,18 @@ var answerD;
 
 var userPick;
 
-var correctAnswer;
+var correctAnswer = 0;
 
-var incorrectAnswer;
+var incorrectAnswer = 0;
 
-var unAnswer;
+var unAnswer = 0;
+
+var question = 0;
 
 var images;
 
-var count=30;
+var count = 30;
 
-// var disneyQuestion = new Array ( );
-
-// disneyQuestion [0] = new Array("In Aladdin, what is the name of Jasmine’s pet tiger?", "Rajah");
-
-// disneyQuestion [1] = new Array ("In Peter Pan, Captain Hook had a hook on which one of his hands?", "His Left Hand");
-
-// disneyQuestion [2] = new Array ("In the Lion King, where does Mufasa and his family live?", "Pride Rock");
-
-// disneyQuestion [3] = new Array ("In Beauty and the Beast, how many eggs does Gaston eat for breakfast?", "5 Dozen");
-
-// disneyQuestion [4] = new Array ("In Alice in Wonderland, what is the name of Alice’s kitten?", "Dinah");
-
-// disneyQuestion [5] = new Array ("After being on earth, where did Hercules first meet his father Zeus?", "In the Temple of Zeus");
-
-// disneyQuestion [6] = new Array ("During the ballroom scene of Beauty & the Beast, what color is Belle’s Gown?", "Gold");
-
-// disneyQuestion [7] = new Array ("In Bambi, what word does the owl use to describe falling in love?", "Twitterpatted");
-
-
-// questions = [{
-//  question: "Which is a fruit?", 
-// choices: ["rock", "paper", "pineapple", "scissors"]
-// answer: 2, }, 
-// {
-//  question: "Which is a colour?",
-//  choices: ["ennui", "green", "umami", "cold"]
-//  answer: 1, 
-//  } ]
 
 var disneyQuestion = [{
     question: "In Aladdin, what is the name of Jasmine's pet tiger?",
@@ -68,17 +42,17 @@ var disneyQuestion = [{
     validAnswer: 1
 
   }, {
-    question:"In Alice in Wonderland, what is the name of Alice’s kitten?",
+    question:"In Alice in Wonderland, what is the name of Alice's kitten?",
     choices: ["Dinah", "Sammie", "Kat", "Luna"],
     validAnswer: 0
-
+ 
   }, {
     question:"After being on earth, where did Hercules first meet his father Zeus?",
     choices: ["Mount Olympus", "Greece", "In the Temple of Zeus", "Elysian Fields"],
     validAnswer: 2
 
   }, {
-    question:"During the ballroom scene of Beauty & the Beast, what color is Belle’s Gown?",
+    question:"During the ballroom scene of Beauty & the Beast, what color is Belle's Gown?",
     choices: ["Yellow", "Blue", "Gold", "White"],
     validAnswer: 2
 
@@ -92,9 +66,9 @@ var disneyQuestion = [{
 ];
 
 $("#start_button").click(function(){
-    $(this).hide();
-    counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+    $(this).hide(); 
     displayTrivia();
+    counter = setInterval(timer, 1000); //1000 will  run it every 1 second
 }); 
 
 
@@ -102,7 +76,6 @@ function timer(){
   count--;
   if (count <= 0) {
      clearInterval(counter);
-     //counter ended, do something here
      return;
   }
 
@@ -111,79 +84,254 @@ function timer(){
 
   
   function displayTrivia() {
+
     $("#question_div").html(disneyQuestion[0].question);
-    // $("#question_div").append("<p>" + disneyQuestion[0].choices + "</p>");
-  
-      var choicesArr = disneyQuestion[0].choices;
-      var buttonsArr = [];
+    question++;
+    var choicesArr = disneyQuestion[0].choices;
+
+    var buttonsArr = [];
 
       for (let i = 0; i < choicesArr.length; i++) {
         var button = $('<button>');
         button.text(choicesArr[i]);
         button.attr('data-id', i);
         $('#choices_div').append(button);
+      }
+  }
 
-        // buttonsArr.push(button);
+    $('#choices_div').on('click', 'button', function(e){
+    userPick = $(this).data("id");
 
-        // var newDiv = $("<div>");
-        // newDiv.attr("class", "buttons_div");
-        // newDiv.append(button);
-       //  newDiv.append(button);
-       // $("#question_div").append(newDiv);
+     if(userPick != disneyQuestion[0].validAnswer) {
+        $('#choices_div').text("Wrong Answer! The correct answer is Rajah.");
+        incorrectAnswer++;
+      } else if (userPick === disneyQuestion[0].validAnswer) {
+        $('#choices_div').text("Correct!!! The pet tiger name is Rajah.");
+        correctAnswer++;
+      }
+  });
+
+setTimeout(function(){ 
+
+  $("#question_div").html(disneyQuestion[1].question);
+  question++;
+  // $("#question_div").append("<p>" + disneyQuestion[0].choices + "</p>");
+
+    var choicesArr = disneyQuestion[1].choices;
+    var buttonsArr = [];
+
+    $('#choices_div').empty();
+
+    for (let i = 0; i < choicesArr.length; i++) {
+      var button = $('<button>');
+      button.text(choicesArr[i]);
+      button.attr('data-id', i);
+      $('#choices_div').append(button);
+    }
+
+    $('#choices_div').on('click', 'button', function(e){
+  // console.log('clicked answer btn');
+      userPick = $(this).data("id");
+      // console.log(userPick);
+      if(userPick != disneyQuestion[1].validAnswer) {
+
+        $('#choices_div').text("Wrong Answer! The correct answer is Left Hand.");
+        incorrectAnswer++;
+        // $("#choices_div").html("<img src=" + disneyQuestion[0].images);
+      } else if (userPick === disneyQuestion[1].validAnswer) {
+          $('#choices_div').text("Correct!!! The answer is Left Hand.");
+          correctAnswer++;
       }
 
-  } // closes displayTrivia
-
-$('#choices_div').on('click', 'button', function(e){
-  console.log('clicked answer btn');
-  userPick = $(this).data("id");
-  // console.log(userPick);
-  disneyQuestion[0].validAnswer;
-  if(userPick != disneyQuestion[0].validAnswer) {
-    // console.log("Wrong");
-    $('#choices_div').text("Wrong Answer! The correct answer is Rajah.");
-    // $("#choices_div").html("<img src=" + disneyQuestion[0].images);
-  } else if (userPick === disneyQuestion[0].validAnswer) {
-    $('#choices_div').text("Correct!!! The pet tiger name is Rajah");
+    });  
 
 
-  }
-  // for (var i = 0; i < choicesArr.length; i++) {
-  //   if(userPick != choicesArr[0]) {
-  //     console.log("wrong!!!");
-  //   }
-  // }
-
-});
-    //Grab question #1 from the object and append it to the game_div section//
-    //Give the uer 30 seconds for the question//
-    //Have the options as a button for user to choose//
-    //If correct answer move on to the result screen, 
-    // (result screen for correct choice will display correct, the images and time left)//
-    //wait for a few seconds later//
-    //Move on to question #2//
-    //If in-correct answer move on to the result screen, wait then move to question #2//
-    // (result screen for in-correct choice will display In-correct, the correct answer, the images and time left)//
-
-    //Time remaining will be 30 seconds and it will refresh everytime the user is on a new screen//
-
-    //When reaches the last question, user scores will display with corect answers, in-correct and un-pick answer, a message, and replay button//
-  
-
-//Press start to start the game
-
-// One question is show at a time with a time ticking down for just one question
-
-// User click the answer and check to see if the answer is correct (if not display the correct answer anyway)
-// (give each answer the same amount of times of 30 seconds.)
-
-// With no user input, the game will move on to the next question automatically.
-
-// When the user take too long to answer show "time up and display the correct answer" move on to the next question
-
-// The timer will stop.
-
-// At the end show the correct and incorrect answer and answer that the user didn't guess. 
+}, 1000 * 10);
 
 
-// When user press start over, the game will reset not restart. 
+setTimeout(function(){ 
+
+  $("#question_div").html(disneyQuestion[2].question);
+  question++;
+
+    var choicesArr = disneyQuestion[2].choices;
+    var buttonsArr = [];
+
+    $('#choices_div').empty();
+
+    for (let i = 0; i < choicesArr.length; i++) {
+      var button = $('<button>');
+      button.text(choicesArr[i]);
+      button.attr('data-id', i);
+      $('#choices_div').append(button);
+    } 
+
+    $('#choices_div').on('click', 'button', function(e){
+      userPick = $(this).data("id");
+
+      if(userPick != disneyQuestion[2].validAnswer) {
+
+        $('#choices_div').text("Wrong Answer! The correct answer is Pride Rock.");
+        incorrectAnswer++;
+
+      } else if (userPick === disneyQuestion[2].validAnswer) {
+        $('#choices_div').text("Correct!!! The answer is Pride Rock.");
+        correctAnswer++;
+      }
+
+    });
+}, 2000 * 10);
+
+setTimeout(function(){ 
+
+  $("#question_div").html(disneyQuestion[3].question);
+  question++;
+
+    var choicesArr = disneyQuestion[3].choices;
+    var buttonsArr = [];
+
+    $('#choices_div').empty();
+
+    for (let i = 0; i < choicesArr.length; i++) {
+      var button = $('<button>');
+      button.text(choicesArr[i]);
+      button.attr('data-id', i);
+      $('#choices_div').append(button);
+    } 
+
+    $('#choices_div').on('click', 'button', function(e){
+      userPick = $(this).data("id");
+
+      if(userPick != disneyQuestion[3].validAnswer) {
+
+        $('#choices_div').text("Wrong Answer! The correct answer is 5 Dozen Eggs.");
+        incorrectAnswer++;
+
+      } else if (userPick === disneyQuestion[3].validAnswer) {
+        $('#choices_div').text("Correct!!! The answer is 5 Dozen Eggs.");
+        correctAnswer++;
+      }
+
+    });
+}, 3000 * 10);
+
+setTimeout(function(){ 
+
+  $("#question_div").html(disneyQuestion[4].question);
+  question++;
+
+    var choicesArr = disneyQuestion[4].choices;
+    var buttonsArr = [];
+
+    for (let i = 0; i < choicesArr.length; i++) {
+      var button = $('<button>');
+      button.text(choicesArr[i]);
+      button.attr('data-id', i);
+      $('#choices_div').append(button);
+    } 
+
+    $('#choices_div').on('click', 'button', function(e){
+      userPick = $(this).data("id");
+
+      if(userPick != disneyQuestion[4].validAnswer) {
+
+        $('#choices_div').text("Wrong Answer! The correct answer is Dinah.");
+        incorrectAnswer++;
+
+      } else if (userPick === disneyQuestion[4].validAnswer) {
+        $('#choices_div').text("Correct!!! Her kitten name is Dinah.");
+        correctAnswer++;
+      }
+
+    });
+}, 4000 * 10);
+
+setTimeout(function(){ 
+
+  $("#question_div").html(disneyQuestion[5].question);
+  question++;
+
+    var choicesArr = disneyQuestion[5].choices;
+    var buttonsArr = [];
+
+    for (let i = 0; i < choicesArr.length; i++) {
+      var button = $('<button>');
+      button.text(choicesArr[i]);
+      button.attr('data-id', i);
+      $('#choices_div').append(button);
+    } 
+
+    $('#choices_div').on('click', 'button', function(e){
+      userPick = $(this).data("id");
+
+      if(userPick != disneyQuestion[5].validAnswer) {
+
+        $('#choices_div').text("Wrong Answer! The correct answer is The Temple of Zeus.");
+        incorrectAnswer++;
+      } else if (userPick === disneyQuestion[5].validAnswer) {
+        $('#choices_div').text("Correct!!! The correct answer is The Temple of Zeus.");
+        correctAnswer++;
+      }
+
+    });
+}, 5000 * 10);
+setTimeout(function(){ 
+
+  $("#question_div").html(disneyQuestion[6].question);
+  question++;
+     
+    var choicesArr = disneyQuestion[6].choices;
+    var buttonsArr = [];
+
+    for (let i = 0; i < choicesArr.length; i++) {
+      var button = $('<button>');
+      button.text(choicesArr[i]);
+      button.attr('data-id', i);
+      $('#choices_div').append(button);
+    } 
+
+    $('#choices_div').on('click', 'button', function(e){
+      userPick = $(this).data("id");
+
+      if(userPick != disneyQuestion[6].validAnswer) {
+
+        $('#choices_div').text("Wrong Answer! The correct answer is Gold.");
+        incorrectAnswer++;
+
+      } else if (userPick === disneyQuestion[6].validAnswer) {
+        $('#choices_div').text("Correct!!! The correct answer is Gold.");
+        correctAnswer++;
+      }
+
+    });
+}, 6000 * 10);
+
+setTimeout(function(){ 
+
+  $("#question_div").html(disneyQuestion[7].question);
+  question++;
+    
+    var choicesArr = disneyQuestion[7].choices;
+    var buttonsArr = [];
+
+    for (let i = 0; i < choicesArr.length; i++) {
+      var button = $('<button>');
+      button.text(choicesArr[i]);
+      button.attr('data-id', i);
+      $('#choices_div').append(button);
+    } 
+
+    $('#choices_div').on('click', 'button', function(e){
+      userPick = $(this).data("id");
+
+      if(userPick != disneyQuestion[7].validAnswer) {
+
+        $('#choices_div').text("Wrong Answer! The correct answer is Twitterpatted.");
+        incorrectAnswer++;
+      } else if (userPick === disneyQuestion[7].validAnswer) {
+        $('#choices_div').text("Correct!!! The correct answer is Twitterpatted.");
+        correctAnswer++;
+      }
+
+    });
+}, 7000 * 10);
